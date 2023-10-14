a4a1c00bbf1442f3fd07757464315cc2f3c38da4
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2023-10-13 21:03:26 -0400

    working on game object

---
+++ pysrc/game.py
@@ -0,0 +1,6 @@
+# FIXME: Change this to a chess interface file and add it to the makefile
+# FIXME: Move all the python library imports to a build lib dir
+from rules import *
+
+class Game(object):
+    def __init__(self, turn)
